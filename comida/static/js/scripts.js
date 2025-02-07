89112e551c0cc00792acd49807385e46fa628df2
Jose-Pacchongo <pjoseedier@gmail.com>
2025-02-07 13:04:05 -0500

    prueba3

--- comida/static/js/scripts.js
+++ comida/static/js/scripts.js
@@ -2,17 +2,20 @@ function loguear() {
     let user = document.getElementById("usuario").value;
     let pass = document.getElementById("clave").value;
 
+    if (!user || !pass) {
+        alert("Por favor, ingrese tanto el usuario como la contraseña.");
+        return;
+    }
+
     const usuarios = {
         "1062077111":"111",
         "1062077222":"222",
         "1062077333":"333"
-    }; 
+    };
 
     if (usuarios[user] && usuarios[user] === pass) {
-        window.location = "home.html";
-        } else {
-            alert("Usuario o contraseña incorrectos");
-        }
-
+        window.location = "pagina"; // O la URL correcta
+    } else {
+        alert("Usuario o contraseña incorrectos");
+    }
 }
-
