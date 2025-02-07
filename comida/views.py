7fb2e57477ef29dbf4fd94e72b001996f8369a71
Jose-Pacchongo <pjoseedier@gmail.com>
2025-02-07 18:12:35 -0500

    prueba3

--- comida/views.py
+++ comida/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect  
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 
-# Create your views here.
+
 def home(request):
     return render(request, 'home.html')
 
@@ -14,11 +16,39 @@ def servicios(request):
 def contactenos(request):
     return render(request, 'contactenos.html')
 
-def iniciar(request):
-        return render(request, 'iniciar.html')
-
 def regis(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        # Crear usuario
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        
+        # Iniciar sesión automáticamente
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('pagina')  
     return render(request, 'regis.html')
+
+def iniciar(request):
+    if request.method == 'POST':
+        username = request.POST['usuario']
+        password = request.POST['clave']
+        
+        # Autenticación con Django
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('pagina')  # Redirige a la página
+        else:
+            # Si las credenciales son incorrectas
+            return render(request, 'iniciar.html', {'error': 'Usuario o contraseña incorrectos'})
+    
+    return render(request, 'iniciar.html')
 
 def pagina(request):
     return render(request, 'pagina.html')
